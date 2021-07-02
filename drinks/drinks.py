94e207583faf21c885aa3cfa4593b4450ac66dd7
Joanne Chen <joannechen2028@gmail.com>
2021-07-02 19:12:35 +0000

    Add milk and fruit tea classes

--- drinks/drinks.py
+++ drinks/drinks.py
@@ -1,5 +1,5 @@
 class Drink:
-    def __init__(self, ice, flavor, sweetness, straw=False):
+    def __init__(self, ice=False, flavor, sweetness, straw=False):
         self.ice = ice
         self.flavor = flavor
         self.sweetness = sweetness
@@ -14,9 +14,19 @@ class Juice(Drink):
         super().__init__(ice, flavor, sweetness, straw=False)
         
 class Tea(Drink):
-    def __init__(self, leaf):
+    def __init__(self, leaf, boba=False):
         self.leaf = leaf
         super().__init__(ice, sweetness, straw=False)
+        
+class Milk_Tea(Tea):
+    def __init__(self, flavor):
+        self.flavor = flavor
+        super().__init__(boba=False)
+        
+class Fruit_Tea(Tea):
+    def __init__(self, flavor):
+        self.flavor = flavor
+        super().__init__()
 
 class Milk(Drink):
     def __init__(self, milk_type):
