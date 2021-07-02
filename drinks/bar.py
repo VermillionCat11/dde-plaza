9d14b457279ece428b05882e996fb84525566c37
Joanne Chen <joannechen2028@gmail.com>
2021-07-02 19:18:16 +0000

    Add flavors and main bar class

---
+++ drinks/bar.py
@@ -0,0 +1,11 @@
+from drinks import Smoothie, Juice, Tea, Milk, Milk_Tea, Fruit_Tea
+
+smoothies = ("Strawberry", "Strawberry-Banana", "Veggie Blend", "Berry Blend", "Tangy Citrus")
+juices = ("orange", "apple", "mango")
+teas = ("black", "green", "milk", "fruit", "jasmine")
+milk_teas = ("plain", "taro", "mung Bean")
+fruit_teas = ("passion fruit", "mango")
+
+class Bar:
+    # Make a menu
+    # Create a serve function
